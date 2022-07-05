b238bef0ee14fbe55d9b26d9a3f18e0020c07531
Thanhbinh1905 <thanhbinh2003195@gmail.com>
2022-07-06 00:42:50 +0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function Changepic(id) {
+    if (id == 'miquang') 
+    {
+        document.getElementById("id1").src = "image/miquang.jpg";
+    } 
+    else if (id == 'nem') 
+    {
+        document.getElementById("id2").src = "image/nem.jpg";
+    }
+}
